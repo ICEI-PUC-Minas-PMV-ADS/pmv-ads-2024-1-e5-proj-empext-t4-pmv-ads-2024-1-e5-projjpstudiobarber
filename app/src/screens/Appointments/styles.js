1a590f7d7c7f63c52c2a8f5f2f5ef75759e289a9
Armando1182 <axamorim@gmail.com>
2024-04-22 20:48:03 -0300

    feat: criacao da tela Appointments

--- app/src/screens/Appointments/styles.js
+++ app/src/screens/Appointments/styles.js
@@ -1,10 +1,44 @@
-import React from 'react';
 import styled from 'styled-components/native';
+import { Dimensions } from 'react-native';
 
 export const Container = styled.SafeAreaView`
-    flex: 1;
-    background-color: #000000;
+    min-height: ${Dimensions.get('window').height}px;
+    background-color: #141416;
 `;
 
+export const Title = styled.Text`
+    color: #fff;
+    font-size: 24px;
+    font-weight: bold;
+    margin: 30px;
+    text-align: center;
+`;
 
-    
+export const InputArea = styled.View`
+    width: 80%;
+    margin: 0 auto;
+    align-items: center;
+    gap: 20px;
+`;
+
+export const Appointment = styled.View`
+    min-width: 100%;
+    background-color: #292a2c;
+    padding: 20px;
+    border-radius: 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+Appointment.Title = styled.Text`
+    color: #fff;
+    font-size: 18px;
+    font-weight: bold;
+    margin-bottom: 10px;
+`;
+
+Appointment.Text = styled.Text`
+    color: #fff;
+    font-size: 16px;
+`;
