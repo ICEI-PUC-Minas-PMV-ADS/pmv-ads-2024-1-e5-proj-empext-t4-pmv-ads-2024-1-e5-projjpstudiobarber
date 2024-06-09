d3455a5a8d9d0ae82a9f37bc712713c3035fd492
rogerpucmg <101529285+rogerpucmg@users.noreply.github.com>
2024-06-09 09:00:46 -0300

    tela de agenda e favoritos
    conteudo e componente

--- app_v1.1/app/src/screens/Favorites/styles.js
+++ app_v1.1/app/src/screens/Favorites/styles.js
@@ -1,10 +1,39 @@
-import React from 'react';
 import styled from 'styled-components/native';
 
 export const Container = styled.SafeAreaView`
-    background-color: #464a4c;
-    flex: 1;
+    background-color: #000000;
+    flex: 1;   
+`;
+
+export const HeaderArea = styled.View`    
+    height: 50px;
     justify-content: center;
-    align-items: center;  
-    
+    padding: 0 20px;
+`;
+
+export const HeaderTitle = styled.Text`
+    font-size: 18px;
+    color: #F6E9C3;
+    font-weight: bold;
+`;
+
+export const Scroller = styled.ScrollView`
+    flex: 1;
+    padding: 0 20px;
+`;
+
+export const LoadingIcon = styled.ActivityIndicator`
+    margin-top: 30px;
+`;
+
+export const ListArea = styled.View`
+    margin-top: 30px;
+    margin-bottom: 30px;
+`;
+
+export const EmptyWarning = styled.Text`
+    text-align: center;
+    margin-top: 30px;
+    color: #F6E9C3;
+    font-size: 14px;
 `;
